ab8fcd2251955f8bfcf20b443c4de045616e62b1
quantum_gecko <galang.izqd@gmail.com>
2025-06-09 16:09:05 +0700

    Update: Marker and Reverse Geocode

--- js/index.js
+++ js/index.js
@@ -1,14 +1,24 @@
 const initMap = (el_id, c, opt) => {
     const map = L.map(el_id).setView([c.lat, c.lng], zoom);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', opt).addTo(map);
-
     return map;
 }
 
-const Marker = () => {
-
+const initMarker = (lat, lng, map) => {
+    return L.marker([lat, lng], { draggable: true }).addTo(map)
 }
 
 const MapRouting = (map, opt) => {
     L.Routing.control(opt).addTo(map);
+}
+
+const ReverseGeocode = async (lat, lon) => {
+    const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`;
+    const response = await fetch(url, {
+        headers: {
+            'User-Agent': 'FATools/1.0 (galang.izqd@gmail.com)'
+        }
+    });
+    const data = await response.json();    
+    return data;
 }
